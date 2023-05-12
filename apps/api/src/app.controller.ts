ae0a34780500427e104ec0f51a3c42faecbecf45
Kene <kayyungx@gmail.com>
2023-05-12 14:35:58 +0100

    inject auth service into app controller

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor(@Inject('AUTH-SERVICE') private authService: ClientProxy) {}
 }
