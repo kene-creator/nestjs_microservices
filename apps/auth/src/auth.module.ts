3a49c1c79b99c61eb7829fa2328429289c06488f
Kene <kayyungx@gmail.com>
2023-05-13 18:00:47 +0100

    connect auth module

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { ConfigModule } from '@nestjs/config';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: './.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('POSTGRES_URI'),
+        // autoLoadEntities: true,
+        // synchronize: true, //! WARNING: This option should never be used in production - otherwise you can lose production data.
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
