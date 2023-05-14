3c28ec5b1c813d6ad1b38ad783d25e75ac062888
Kene <kayyungx@gmail.com>
2023-05-14 16:08:14 +0100

    update auth service functions

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepsitory: Repository<UserEntity>,
+  ) {}
+
+  async getUsers() {
+    return await this.userRepsitory.find();
+  }
+
+  async postUser() {
+    return await this.userRepsitory.save({
+      name: 'USERR',
+    });
   }
 }
