1c3b3c94c36b81e32784ec544a8fbd6f10c302b3
Kene <kayyungx@gmail.com>
2023-05-15 19:54:28 +0100

    update auth module

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -5,28 +5,32 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { dataSource, dataSourceOption } from './db/data-source';
+import { SharedModule } from '@app/shared';
+import { PostgresDBModule } from '@app/shared/postgredb.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: './.env',
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      // useFactory: (configService: ConfigService) => ({
-      //   type: 'postgres',
-      //   url: configService.get('POSTGRES_URI'),
-      //   autoLoadEntities: true,
-      //   synchronize: true, //! WARNING: This option should never be used in production - otherwise you can lose production data.
-      // }),
-      useFactory: (configService: ConfigService) => ({
-        ...dataSourceOption,
-        url: configService.get('POSTGRES_URI'),
-        autoLoadEntities: true,
-        synchronize: true, //! WARNING: This option should never be used in production - otherwise you can lose production data.
-      }),
-      inject: [ConfigService],
-    }),
+    SharedModule,
+    PostgresDBModule,
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //    useFactory: (configService: ConfigService) => ({
+    //      type: 'postgres',
+    //      url: configService.get('POSTGRES_URI'),
+    //      autoLoadEntities: true,
+    //      synchronize: true, //! WARNING: This option should never be used in production - otherwise you can lose production data.
+    //    }),
+    //   useFactory: (configService: ConfigService) => ({
+    //     ...dataSourceOption,
+    //     url: configService.get('POSTGRES_URI'),
+    //     autoLoadEntities: true,
+    //     synchronize: true, //! WARNING: This option should never be used in production - otherwise you can lose production data.
+    //   }),
+    //   inject: [ConfigService],
+    // }),
     TypeOrmModule.forFeature([UserEntity]),
   ],
   controllers: [AuthController],
