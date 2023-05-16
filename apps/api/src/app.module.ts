bad4f3d6a51bf73693e5956523b08fbd09b63219
Kene <kayyungx@gmail.com>
2023-05-16 19:05:34 +0100

    update app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SharedModule } from '@app/shared';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -10,13 +11,58 @@ import { SharedModule } from '@app/shared';
       isGlobal: true,
       envFilePath: './.env',
     }),
-    SharedModule.registerRmq('AUTH_SERIVICE', process.env.RABBITMQ_AUTH_QUEUE),
-    SharedModule.registerRmq(
-      'PRESENCE_SERIVICE',
-      process.env.RABBITMQ_PRESENCE_QUEUE,
-    ),
+    // SharedModule.registerRmq('AUTH_SERIVICE', process.env.RABBITMQ_AUTH_QUEUE),
+    // SharedModule.registerRmq(
+    //   'PRESENCE_SERIVICE',
+    //   process.env.RABBITMQ_PRESENCE_QUEUE,
+    // ),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'AUTH_SERVICE',
+      useFactory: (configService: ConfigService) => {
+        const USER = configService.get('RABBITMQ_USER');
+        const PASSWORD = configService.get('RABBITMQ_PASS');
+        const HOST = configService.get('RABBITMQ_HOST');
+        const QUEUE = configService.get('RABBITMQ_AUTH_QUEUE');
+
+        return ClientProxyFactory.create({
+          transport: Transport.RMQ,
+          options: {
+            urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
+            queue: QUEUE,
+            queueOptions: {
+              durable: true, //? This option is used to persist the queue on the RabbitMQ server.
+            },
+          },
+        });
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: 'PRESENCE_SERVICE',
+      useFactory: (configService: ConfigService) => {
+        const USER = configService.get('RABBITMQ_USER');
+        const PASSWORD = configService.get('RABBITMQ_PASS');
+        const HOST = configService.get('RABBITMQ_HOST');
+        const QUEUE = configService.get('RABBITMQ_PRESENCE_QUEUE');
+        console.log(USER, PASSWORD, HOST, QUEUE);
+
+        return ClientProxyFactory.create({
+          transport: Transport.RMQ,
+          options: {
+            urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
+            queue: QUEUE,
+            queueOptions: {
+              durable: true, //? This option is used to persist the queue on the RabbitMQ server.
+            },
+          },
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
 })
 export class AppModule {}
