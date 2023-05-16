2164085c31fd2d0632fed19c9077907127e131e3
Kene <kayyungx@gmail.com>
2023-05-16 19:05:48 +0100

    setup presence

--- apps/presence/src/presence.controller.ts
+++ apps/presence/src/presence.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get } from '@nestjs/common';
 import { PresenceService } from './presence.service';
 import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
 import { SharedService } from '@app/shared';
+import { AuthGuard } from '@app/shared/auth.guard';
 
 @Controller()
 export class PresenceController {
   constructor(
     private readonly presenceService: PresenceService,
     private readonly sharedService: SharedService,
+    private readonly authGuard: AuthGuard,
   ) {}
 
   @MessagePattern({ cmd: 'get-presence' })
