544e5c64238e30d0d35ad448f7bad54168a0cab2
Kene <kayyungx@gmail.com>
2023-05-18 15:11:02 +0100

    correct spelling in app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -10,9 +10,9 @@ import { SharedModule } from '@app/shared';
       isGlobal: true,
       envFilePath: './.env',
     }),
-    SharedModule.registerRmq('AUTH_SERIVICE', process.env.RABBITMQ_AUTH_QUEUE),
+    SharedModule.registerRmq('AUTH_SERVICE', process.env.RABBITMQ_AUTH_QUEUE),
     SharedModule.registerRmq(
-      'PRESENCE_SERIVICE',
+      'PRESENCE_SERVICE',
       process.env.RABBITMQ_PRESENCE_QUEUE,
     ),
   ],
