269a4b5a35b266878abb17859e4c5731ede8f5ff
Kene <kayyungx@gmail.com>
2023-05-18 12:43:07 +0100

    introduce the register service function

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { Repository } from 'typeorm';
+import { NewUserDto } from './dto/new-user.dto';
+
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -14,9 +17,38 @@ export class AuthService {
     return await this.userRepsitory.find();
   }
 
-  async postUser() {
-    return await this.userRepsitory.save({
-      name: 'USERR',
+  async register(newUser: Readonly<NewUserDto>): Promise<UserEntity> {
+    const { email, firstName, lastName, password } = newUser;
+
+    const existingUser = await this.getUserByEmail(email);
+
+    if (existingUser) {
+      throw new ConflictException(
+        `Already registered user with email ${email}`,
+      );
+    }
+
+    const hashedPassword = await this.hashPassword(password);
+
+    const savedUser = await this.userRepsitory.save({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
     });
+
+    delete savedUser.password;
+    return savedUser;
+  }
+
+  async getUserByEmail(email: string): Promise<UserEntity> {
+    return await this.userRepsitory.findOne({
+      where: { email },
+      select: ['id', 'firstName', 'lastName', 'email', 'password'],
+    });
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 12);
   }
 }
