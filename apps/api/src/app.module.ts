25ecb343001b4bb9f6dc5ba694612e39e9da3995
Kene <kayyungx@gmail.com>
2023-05-18 09:45:27 +0100

    update shared module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { SharedModule } from '@app/shared';
-import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -11,58 +10,58 @@ import { ClientProxyFactory, Transport } from '@nestjs/microservices';
       isGlobal: true,
       envFilePath: './.env',
     }),
-    // SharedModule.registerRmq('AUTH_SERIVICE', process.env.RABBITMQ_AUTH_QUEUE),
-    // SharedModule.registerRmq(
-    //   'PRESENCE_SERIVICE',
-    //   process.env.RABBITMQ_PRESENCE_QUEUE,
-    // ),
+    SharedModule.registerRmq('AUTH_SERIVICE', process.env.RABBITMQ_AUTH_QUEUE),
+    SharedModule.registerRmq(
+      'PRESENCE_SERIVICE',
+      process.env.RABBITMQ_PRESENCE_QUEUE,
+    ),
   ],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'AUTH_SERVICE',
-      useFactory: (configService: ConfigService) => {
-        const USER = configService.get('RABBITMQ_USER');
-        const PASSWORD = configService.get('RABBITMQ_PASS');
-        const HOST = configService.get('RABBITMQ_HOST');
-        const QUEUE = configService.get('RABBITMQ_AUTH_QUEUE');
+    // {
+    //   provide: 'AUTH_SERVICE',
+    //   useFactory: (configService: ConfigService) => {
+    //     const USER = configService.get('RABBITMQ_USER');
+    //     const PASSWORD = configService.get('RABBITMQ_PASS');
+    //     const HOST = configService.get('RABBITMQ_HOST');
+    //     const QUEUE = configService.get('RABBITMQ_AUTH_QUEUE');
 
-        return ClientProxyFactory.create({
-          transport: Transport.RMQ,
-          options: {
-            urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
-            queue: QUEUE,
-            queueOptions: {
-              durable: true, //? This option is used to persist the queue on the RabbitMQ server.
-            },
-          },
-        });
-      },
-      inject: [ConfigService],
-    },
-    {
-      provide: 'PRESENCE_SERVICE',
-      useFactory: (configService: ConfigService) => {
-        const USER = configService.get('RABBITMQ_USER');
-        const PASSWORD = configService.get('RABBITMQ_PASS');
-        const HOST = configService.get('RABBITMQ_HOST');
-        const QUEUE = configService.get('RABBITMQ_PRESENCE_QUEUE');
-        console.log(USER, PASSWORD, HOST, QUEUE);
+    //     return ClientProxyFactory.create({
+    //       transport: Transport.RMQ,
+    //       options: {
+    //         urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
+    //         queue: QUEUE,
+    //         queueOptions: {
+    //           durable: true, //? This option is used to persist the queue on the RabbitMQ server.
+    //         },
+    //       },
+    //     });
+    //   },
+    //   inject: [ConfigService],
+    // },
+    // {
+    //   provide: 'PRESENCE_SERVICE',
+    //   useFactory: (configService: ConfigService) => {
+    //     const USER = configService.get('RABBITMQ_USER');
+    //     const PASSWORD = configService.get('RABBITMQ_PASS');
+    //     const HOST = configService.get('RABBITMQ_HOST');
+    //     const QUEUE = configService.get('RABBITMQ_PRESENCE_QUEUE');
+    //     console.log(USER, PASSWORD, HOST, QUEUE);
 
-        return ClientProxyFactory.create({
-          transport: Transport.RMQ,
-          options: {
-            urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
-            queue: QUEUE,
-            queueOptions: {
-              durable: true, //? This option is used to persist the queue on the RabbitMQ server.
-            },
-          },
-        });
-      },
-      inject: [ConfigService],
-    },
+    //     return ClientProxyFactory.create({
+    //       transport: Transport.RMQ,
+    //       options: {
+    //         urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
+    //         queue: QUEUE,
+    //         queueOptions: {
+    //           durable: true, //? This option is used to persist the queue on the RabbitMQ server.
+    //         },
+    //       },
+    //     });
+    //   },
+    //   inject: [ConfigService],
+    // },
   ],
 })
 export class AppModule {}
--- libs/shared/src/shared.module.ts
+++ libs/shared/src/shared.module.ts
@@ -16,30 +16,33 @@ import { AuthGuard } from './auth.guard';
 })
 export class SharedModule {
   static registerRmq(service: string, queue: string): DynamicModule {
+    const providers = [
+      {
+        provide: service,
+        useFactory: (configService: ConfigService) => {
+          const USER = configService.get('RABBITMQ_USER');
+          const PASSWORD = configService.get('RABBITMQ_PASS');
+          const HOST = configService.get('RABBITMQ_HOST');
+
+          return ClientProxyFactory.create({
+            transport: Transport.RMQ,
+            options: {
+              urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
+              queue,
+              queueOptions: {
+                durable: true, //? This option is used to persist the queue on the RabbitMQ server.
+              },
+            },
+          });
+        },
+        inject: [ConfigService],
+      },
+    ];
+
     return {
       module: SharedModule,
-      providers: [
-        {
-          provide: service,
-          useFactory: (configService: ConfigService) => {
-            const USER = configService.get('RABBITMQ_USER');
-            const PASSWORD = configService.get('RABBITMQ_PASS');
-            const HOST = configService.get('RABBITMQ_HOST');
-
-            return ClientProxyFactory.create({
-              transport: Transport.RMQ,
-              options: {
-                urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
-                queue,
-                queueOptions: {
-                  durable: true, //? This option is used to persist the queue on the RabbitMQ server.
-                },
-              },
-            });
-          },
-          inject: [ConfigService],
-        },
-      ],
+      providers,
+      exports: providers,
     };
   }
 }
