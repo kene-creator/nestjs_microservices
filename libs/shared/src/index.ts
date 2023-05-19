6439649f51da37ea7d226ac6a76bb9f827be3e35
Kene <kayyungx@gmail.com>
2023-05-19 21:47:24 +0100

    update exports

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,4 +1,21 @@
+//? modules
 export * from './shared.module';
-export * from './shared.service';
-export * from './auth.guard';
 export * from './postgredb.module';
+
+//? services
+export * from './shared.service';
+
+//? guards
+export * from './auth.guard';
+
+//? repositories
+export * from './repositories/base/base.interface.repository';
+export * from './repositories/base/base.abstract.repository';
+export * from './repositories/user.repository';
+
+//? entities
+export * from './entities/user.entity';
+
+//? interface
+export * from './interface/users.repository.interface';
+export * from './interface/shared.service.interface';
