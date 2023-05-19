c4c219d27f94d6b005c3bd04dfb48700dce8c23a
Kene <kayyungx@gmail.com>
2023-05-19 13:40:59 +0100

    update jwt strategy

---
+++ apps/auth/src/jwt-strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtRequest } from './jwt-request.interface';
+
+Injectable();
+
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: JwtRequest) => {
+          return request?.jwt;
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    return { ...payload };
+  }
+}
