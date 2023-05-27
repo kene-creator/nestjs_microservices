625e86bdac6225c16ead071700bd6985401b08da
Kene <kayyungx@gmail.com>
2023-05-27 22:56:54 +0100

    add user interceptos, interface and repository

---
+++ libs/shared/src/interceptors/user.interceptors.ts
@@ -0,0 +1,45 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+
+import { ClientProxy } from '@nestjs/microservices';
+
+import { Observable, switchMap, catchError, tap } from 'rxjs';
+
+import { UserJwt } from '../interface/user-jwt.interface';
+
+@Injectable()
+export class UserInterceptor implements NestInterceptor {
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly authService: ClientProxy,
+  ) {}
+
+  intercept(ctx: ExecutionContext, next: CallHandler): Observable<any> {
+    if (ctx.getType() !== 'http') return next.handle();
+
+    const request = ctx.switchToHttp().getRequest();
+    const authHeader = request.headers['authorization'];
+
+    if (!authHeader) return next.handle();
+
+    const authHeaderParts = authHeader.split(' ');
+
+    if (authHeaderParts.length !== 2) return next.handle();
+
+    const [, jwt] = authHeaderParts;
+
+    return this.authService.send({ cmd: 'decode-jwt' }, { jwt }).pipe(
+      tap((value) => console.log('Value emitted:', value)),
+      switchMap(({ user }) => {
+        console.log(4, user);
+        request.user = user;
+        return next.handle();
+      }),
+      catchError(() => next.handle()),
+    );
+  }
+}
