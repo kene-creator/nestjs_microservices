0db67f5b6342a8621cfe7f36edc00b0c156e9550
Kene <kayyungx@gmail.com>
2023-05-31 15:56:10 +0100

    setup presence gateway

---
+++ apps/presence/src/presence.gateway.ts
@@ -0,0 +1,143 @@
+import { Inject } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { ClientProxy } from '@nestjs/microservices';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+
+import { Server, Socket } from 'socket.io';
+import { firstValueFrom } from 'rxjs';
+import { Cache } from 'cache-manager';
+
+import { FriendRequestEntity } from '@app/shared/entities/friend-request.entity';
+import { UserRequest } from '@app/shared/interface/user-request.interface';
+
+import { ActiveUser } from './interfaces/ActiveUser.interface';
+
+@WebSocketGateway({ cors: true })
+export class PresenceGateway {
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly authService: ClientProxy,
+    @Inject(CACHE_MANAGER) private readonly cache: Cache,
+  ) {}
+
+  @WebSocketServer()
+  server: Server;
+
+  //! Remove in Production
+  async onModuleInit() {
+    await this.cache.reset();
+  }
+
+  private async getFriends(userId: number) {
+    const ob$ = this.authService.send<FriendRequestEntity[]>(
+      { cmd: 'get-friends' },
+      { userId },
+    );
+    const res = await firstValueFrom(ob$).catch((err) => console.error(err));
+
+    if (!res) return;
+
+    const friends = res.map((friendRequest) => {
+      const isUserCreator = userId === friendRequest.creator.id;
+
+      const friendDetials = isUserCreator
+        ? friendRequest.receiver
+        : friendRequest.creator;
+
+      const { id, firstName, lastName, email } = friendDetials;
+
+      return {
+        id,
+        firstName,
+        lastName,
+        email,
+      };
+    });
+
+    return friends;
+  }
+
+  private async emitStatusToFriends(activeUser: ActiveUser) {
+    const friends = await this.getFriends(activeUser.id);
+
+    for (const f of friends) {
+      const user = this.cache.get(`user ${f.id}`);
+
+      if (!user) continue;
+
+      const friend = user as unknown as ActiveUser;
+
+      this.server.to(friend.socketId).emit('friendActive', {
+        id: activeUser.id,
+        isActive: activeUser.isActive,
+      });
+
+      if (activeUser.isActive) {
+        this.server.to(activeUser.socketId).emit('friendActive', {
+          id: friend.id,
+          isActive: friend.isActive,
+        });
+      }
+    }
+  }
+
+  async handleDisconnect(socket: Socket) {
+    console.log('HANDLE DISCONNECT');
+
+    await this.setActiveStatus(socket, false);
+  }
+
+  private async setActiveStatus(socket: Socket, isActive: boolean) {
+    const user = socket.data?.user;
+
+    if (!user) return;
+
+    const activeUser: ActiveUser = {
+      id: user.id,
+      socketId: socket.id,
+      isActive,
+    };
+
+    await this.cache.set(`user ${user.id}`, activeUser, 0);
+    await this.emitStatusToFriends(activeUser);
+  }
+
+  async handleConnection(socket: Socket) {
+    console.log('handle connection');
+    const jwt = socket.handshake.headers.authorization ?? null;
+
+    if (!jwt) {
+      this.handleDisconnect(socket);
+      return;
+    }
+
+    const ob$ = this.authService.send<UserRequest>(
+      { cmd: 'decode-jwt' },
+      { jwt },
+    );
+    const res = await firstValueFrom(ob$).catch((err) => console.error(err));
+
+    if (!res || !res?.user) {
+      this.handleDisconnect(socket);
+      return;
+    }
+
+    const { user } = res;
+
+    socket.data.user = user;
+
+    await this.setActiveStatus(socket, true);
+  }
+
+  @SubscribeMessage('updateActiveStatus')
+  async updateActiveStatus(socket: Socket, isActive: boolean) {
+    if (!socket.data?.user) return;
+
+    await this.setActiveStatus(socket, isActive);
+  }
+}
