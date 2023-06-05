f884e201ea09b4a1f62088a5535e1c37ff1b4b83
Kene <kayyungx@gmail.com>
2023-06-05 11:59:39 +0100

    update redis set cache ttl

--- libs/shared/src/services/redis.service.ts
+++ libs/shared/src/services/redis.service.ts
@@ -11,13 +11,18 @@ export default class RedisService {
     return await this.cache.get(key);
   }
 
-  async set(key: string, value: unknown) {
+  async set(key: string, value: unknown, ttl = 0) {
     console.log(`set ${key}`);
-    return await this.cache.set(key, value);
+    return await this.cache.set(key, value, ttl);
   }
 
   async del(key: string) {
     console.log(`del ${key}`);
     return await this.cache.del(key);
+  }
+
+  async reset() {
+    console.log('reset cache');
+    return await this.cache.reset();
   }
 }
