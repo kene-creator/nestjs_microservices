503a41bf52e9aefc336abe9d5816fe0b19e02cc7
Kene <kayyungx@gmail.com>
2023-06-11 19:40:59 +0100

    update chat service

--- apps/chat/src/chat.service.ts
+++ apps/chat/src/chat.service.ts
@@ -1,8 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { UserEntity } from '@app/shared';
+import { ConversationsRepositoryInterface } from '@app/shared/interface/conversation.repository.interface';
+import { MessagesRepositoryInterface } from '@app/shared/interface/message.repository.interface';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class ChatService {
+  constructor(
+    @Inject('ConversationsRepositoryInterface')
+    private readonly conversationsRepository: ConversationsRepositoryInterface,
+    @Inject('MessagesRepositoryInterface')
+    private readonly messagesRepository: MessagesRepositoryInterface,
+    @Inject('AUTH_SERVICE') private readonly authService: ClientProxy,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  private async getUser(id: number) {
+    const ob$ = this.authService.send<UserEntity>(
+      {
+        cmd: 'get-user',
+      },
+      { id },
+    );
+
+    const user = await firstValueFrom(ob$).catch((err) => console.error(err));
+
+    return user;
+  }
+
+  async getConversations(userId: number) {
+    const allConversations =
+      await this.conversationsRepository.findWithRelations({
+        relations: ['users'],
+      });
+
+    const userConversations = allConversations.filter((conversation) => {
+      const userIds = conversation.users.map((user) => user.id);
+      return userIds.includes(userId);
+    });
+
+    return userConversations.map((conversation) => ({
+      id: conversation.id,
+      userIds: (conversation?.users ?? []).map((user) => user.id),
+    }));
   }
 }
