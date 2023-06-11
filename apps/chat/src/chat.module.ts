b57f3db4fb81cc136b290880fa731304c1000afd
Kene <kayyungx@gmail.com>
2023-06-11 19:40:46 +0100

    update chat module

--- apps/chat/src/chat.module.ts
+++ apps/chat/src/chat.module.ts
@@ -1,10 +1,46 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { PostgresDBModule, SharedModule, UserEntity } from '@app/shared';
+
+import { ChatGateway } from './chat.gateway';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { FriendRequestEntity } from '@app/shared/entities/friend-request.entity';
+import { ConversationEntity } from '@app/shared/entities/conversation.entity';
+import { MessageEntity } from '@app/shared/entities/message.entity';
+import { RedisModule } from '@app/shared/modules/redis.module';
+import { ConversationsRepository } from '@app/shared/repositories/conversation.repository';
+import { MessagesRepository } from '@app/shared/repositories/message.repository';
 
 @Module({
-  imports: [],
+  imports: [
+    PostgresDBModule,
+    RedisModule,
+    SharedModule.registerRmq('AUTH_SERVICE', process.env.RABBITMQ_AUTH_QUEUE),
+    SharedModule.registerRmq(
+      'PRESENCE_SERVICE',
+      process.env.RABBITMQ_PRESENCE_QUEUE,
+    ),
+    TypeOrmModule.forFeature([
+      UserEntity,
+      FriendRequestEntity,
+      ConversationEntity,
+      MessageEntity,
+    ]),
+  ],
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [
+    ChatService,
+    ChatGateway,
+    {
+      provide: 'ConversationsRepositoryInterface',
+      useClass: ConversationsRepository,
+    },
+    {
+      provide: 'MessagesRepositoryInterface',
+      useClass: MessagesRepository,
+    },
+  ],
 })
 export class ChatModule {}
